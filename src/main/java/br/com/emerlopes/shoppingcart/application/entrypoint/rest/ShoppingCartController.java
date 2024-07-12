8bd70c8c1ca90bfc693cdf97f430f92201841db9
Emerson <lopes.ds@live.com>
2024-07-12 12:53:49 -0300

    feat: criando rota para criar carrinho de compras

---
+++ src/main/java/br/com/emerlopes/shoppingcart/application/entrypoint/rest/ShoppingCartController.java
@@ -0,0 +1,38 @@
+package br.com.emerlopes.shoppingcart.application.entrypoint.rest;
+
+import br.com.emerlopes.shoppingcart.application.shared.CustomResponseDTO;
+import br.com.emerlopes.shoppingcart.domain.usecase.CreateShoppingCartByUsernameUseCase;
+import br.com.emerlopes.shoppingcart.domain.usecase.GetShoppingCartByUsernameUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shopping-carts")
+public class ShoppingCartController {
+
+    private final CreateShoppingCartByUsernameUseCase createShoppingCartByUsernameUseCase;
+    private final GetShoppingCartByUsernameUseCase getShoppingCartByUsernameUseCase;
+
+    public ShoppingCartController(
+            final CreateShoppingCartByUsernameUseCase createShoppingCartByUsernameUseCase,
+            final GetShoppingCartByUsernameUseCase getShoppingCartByUsernameUseCase
+    ) {
+        this.createShoppingCartByUsernameUseCase = createShoppingCartByUsernameUseCase;
+        this.getShoppingCartByUsernameUseCase = getShoppingCartByUsernameUseCase;
+    }
+
+    @PostMapping("/register/{username}")
+    public ResponseEntity<?> registerShoppingCart(
+            final @PathVariable("username") String username
+    ) {
+
+        final var executionResult = createShoppingCartByUsernameUseCase.execute(username);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new CustomResponseDTO<>().setData(executionResult)
+        );
+    }
+}
