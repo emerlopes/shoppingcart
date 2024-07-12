5049516fdc1afa60bd5ad8b3f8035940f9e97fa1
Emerson <lopes.ds@live.com>
2024-07-12 17:23:20 -0300

    chore: implementando remocao de carrinho e produto.

--- src/main/java/br/com/emerlopes/shoppingcart/domain/repository/ShoppingCartDomainRepository.java
+++ src/main/java/br/com/emerlopes/shoppingcart/domain/repository/ShoppingCartDomainRepository.java
@@ -1,5 +1,6 @@
 package br.com.emerlopes.shoppingcart.domain.repository;
 
+import br.com.emerlopes.shoppingcart.application.exceptions.UsernameNotFoundException;
 import br.com.emerlopes.shoppingcart.domain.entity.ProductDomainEntity;
 import br.com.emerlopes.shoppingcart.domain.entity.ShoppingCartDomainEntity;
 
@@ -19,4 +20,14 @@ public interface ShoppingCartDomainRepository {
     );
 
     List<ShoppingCartDomainEntity> getAllShoppingCarts();
+
+    ShoppingCartDomainEntity removeProductFromShoppingCart(
+            final ProductDomainEntity productDomainEntity
+    );
+
+    void deleteShoppingCart(
+            final String username
+    ) throws UsernameNotFoundException;
+
+
 }
