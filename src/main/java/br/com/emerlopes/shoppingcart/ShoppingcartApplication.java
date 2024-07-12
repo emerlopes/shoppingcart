69a8137e164742cc8e51068ff4d6acf7d0c3f0a2
Emerson <lopes.ds@live.com>
2024-07-12 12:50:42 -0300

    chore: implementando anotacao EnableFeignClients

--- src/main/java/br/com/emerlopes/shoppingcart/ShoppingcartApplication.java
+++ src/main/java/br/com/emerlopes/shoppingcart/ShoppingcartApplication.java
@@ -2,8 +2,10 @@ package br.com.emerlopes.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients({"br.com.emerlopes.shoppingcart.infrastructure.integrations"})
 public class ShoppingcartApplication {
 
     public static void main(String[] args) {
