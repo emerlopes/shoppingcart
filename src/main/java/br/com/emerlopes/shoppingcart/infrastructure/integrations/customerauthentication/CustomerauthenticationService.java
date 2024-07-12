769263b2da67d7eb2e1ce3ccc3ad8c2a3ebdf954
Emerson <lopes.ds@live.com>
2024-07-12 12:49:19 -0300

    chore: removendo classe

--- src/main/java/br/com/emerlopes/shoppingcart/infrastructure/integrations/customerauthentication/CustomerauthenticationService.java
+++
@@ -1,8 +0,0 @@
-package br.com.emerlopes.shoppingcart.infrastructure.integrations.customerauthentication;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomerauthenticationService {
-}
