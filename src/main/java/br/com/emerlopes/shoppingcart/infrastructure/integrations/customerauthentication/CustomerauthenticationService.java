ab7eb17ade2d1c2de8dcae653cf4be57c94402c1
Emerson <lopes.ds@live.com>
2024-07-12 11:45:13 -0300

    chore: implementando integracao com CustomerauthenticationService

---
+++ src/main/java/br/com/emerlopes/shoppingcart/infrastructure/integrations/customerauthentication/CustomerauthenticationService.java
@@ -0,0 +1,8 @@
+package br.com.emerlopes.shoppingcart.infrastructure.integrations.customerauthentication;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerauthenticationService {
+}
