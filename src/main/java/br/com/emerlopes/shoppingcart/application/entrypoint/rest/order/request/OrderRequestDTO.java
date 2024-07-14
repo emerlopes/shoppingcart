29ff9c50456062cdeebbe9cddacd989bd70e9b72
Emerson <lopes.ds@live.com>
2024-07-14 14:59:24 -0300

    chore: JsonIgnoreProperties

--- src/main/java/br/com/emerlopes/shoppingcart/application/entrypoint/rest/order/request/OrderRequestDTO.java
+++ src/main/java/br/com/emerlopes/shoppingcart/application/entrypoint/rest/order/request/OrderRequestDTO.java
@@ -1,6 +1,7 @@
 package br.com.emerlopes.shoppingcart.application.entrypoint.rest.order.request;
 
 import br.com.emerlopes.shoppingcart.application.entrypoint.rest.dto.request.ProductRequestDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderRequestDTO {
     private String username;
     private List<ProductRequestDTO> products;
